41ad0f9642cda826980485b22b57249fea0bf187
Mohamed Akl <mohamedakl96@gmail.com>
2017-11-16 15:23:41 +0200

    commit

--- signup.php
+++ signup.php
@@ -1,7 +1,9 @@
 <?php
+session_start();
+require_once('file el feh el database connection.php');
 $username=$_POST['username'];
 $password=$_POST['password'];
 $email=$_POST['email'];
 
 
- ?>
+?>
--- univ.sql
+++ univ.sql
@@ -4,9 +4,9 @@ create TABLE Department
   dept_id int auto_increment not null unique ,
   primary key (dept_id),
   dept_name varchar(50) unique  not null  ,
-  dept_describtion varchar(255) 
+  dept_describtion varchar(255)
   );
-  
+
 create table user
 (
   dept_id int,
@@ -16,9 +16,9 @@ create table user
   e_mail varchar(50) ,
   user_password varchar(50) default '12345678' ,
   foreign key (dept_id) references Department(dept_id),
-  username varchar(50) not null 
+  username varchar(50) not null
   );
-  
+
 create table course
 (
   dept_id int,
@@ -30,34 +30,31 @@ create table course
   credit_hours int not null ,
   foreign key(dept_id) references Department(dept_id)
   );
-  
-  
-  insert into Department 
-  values (1,'computer_engineergin',' ');
-  
-  
-  insert into Department 
+
+
+  insert into Department
+  values (1,'computer_engineerging',' ');
+
+
+  insert into Department
   values (2,'marketing',' ');
-  
-  
-  
-  insert into course 
+
+
+
+  insert into course
   values (1,1,'systems',' ','zakarya',3 );
-  
-  insert into course 
+
+  insert into course
   values (1,2,'DSP',' ','ibrahim',3);
-  
-  insert into course 
+
+  insert into course
   values (1,3,'algorithms',' ','menyawy',3 );
-  
-  insert into course 
+
+  insert into course
   values (2,4,'marketing101',' ','walid',3 );
-  
-  insert into course 
+
+  insert into course
   values (2,5,'production',' ','yehia',3 );
-  
-  insert into course 
+
+  insert into course
   values (2,6,'distribution',' ','sherif',3 );
-  
-  
-  
