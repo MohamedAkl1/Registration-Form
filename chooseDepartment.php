6f9366935fcb7d629ff890e42b5a586b92a5227a
Mohamed Akl <mohamedakl96@gmail.com>
2017-11-16 15:47:28 +0200

    commit

--- chooseDepartment.php
+++ chooseDepartment.php
@@ -2,13 +2,12 @@
 session_start();
 require_once('database.php');
 $i=0;
-require_once('config.php');
 echo "welcome"." ".$_SESSION['username']."<br>";
-$query="SELECT * FROM departments";
+$query="SELECT * FROM department";
 $result=mysqli_query($conn,$query);
 while ($row=mysqli_fetch_array($result)){
     echo "<ul class=\"list-group\">";
-   echo "<li><a href='coursesPage.php?id=".$row['department_id']."'".">".$row['department_name']."</a></li>";
+   echo "<li><a href='coursesPage.php?id=".$row['dept_id']."'".">".$row['dept_name']."</a></li>";
     echo "</ul>";
    echo "<br>";
 }
