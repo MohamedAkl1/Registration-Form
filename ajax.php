a5c329aad125ca43fd72c82d5b26038acb46ad19
Mohamed Akl <mohamedakl96@gmail.com>
2017-11-16 16:33:00 +0200

    final commit

---
+++ ajax.php
@@ -0,0 +1,43 @@
+<<?php
+
+function checkEmail(email) {
+
+
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(email)) {
+       // alert('Please provide a valid email address');
+        email.focus;
+        return false;
+    }
+    else
+        return true;
+}
+
+
+function myFunction() {
+    var name = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (name == '' || email == '' || password == '' || name== null || email==null || password==null || checkEmail(email)== false) {
+        if(checkEmail(email)== false)
+            alert('Please provide a valid email address');
+        else
+        alert("Please Fill All Fields")
+    } else {
+// AJAX code to submit form.
+        $.ajax({
+            type: "POST",
+            url: "registeration.php",
+            data: $('#form').serialize(),
+            cache: false,
+            success: function(html) {
+                alert(html);
+            }
+        });
+    }
+    return false;
+}
+
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+session_start();
+require_once ('database.php');
+if (isset($_POST['email'],$_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $query = "SELECT username,dept_id FROM user WHERE e_mail='" . $email . "' AND user_password='" . $password . "' ";
+    $result = mysqli_query($conn, $query);
+    $row=mysqli_fetch_array($result);
+    echo $conn->error;
+
+    if (mysqli_num_rows($result) == 0) {
+        echo "<div class='container'>";
+        echo"<div class=\"alert alert-danger\">
+ <strong>Wrong!</strong> check email and password
+</div>";
+       echo"</div>";
+    } else {
+            $_SESSION['username']=$row['username'];
+            if($row['dept_id']== null){
+                header('location:chooseDepartment.php');
+            }else{
+                header('location:coursesPage.php?id='.$row['dept_id']);
+            }
+    }
+}
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -61,15 +89,15 @@
     </form>
   </div>
 
-  <form>
+  <form method="post" action="index.php" >
    <div class="container">
   <div class="form-group">
-    <label for="exampleInputEmail1">Username</label>
-    <input type="text" class="form-control" id="signInUsername"  placeholder="Enter Username"  required>
+    <label for="exampleInputEmail1">Email</label>
+    <input type="text" class="form-control" name="email" id="signInUsername"  placeholder="Enter Username"  required>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="signInPassword" placeholder="Password" required>
+    <input type="password" class="form-control" name="password" id="signInPassword" placeholder="Password" required>
   </div>
 
   <button type="submit" class="btn btn-primary">Submit</button>
